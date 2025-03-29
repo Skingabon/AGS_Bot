85d22558a34ead7cccde8ac0480ed2fd7c843e8d
skingabon <reclama.katkov@gmail.com>
2025-03-29 15:06:05 +0300

    дополнил лоигку. но пока не всё

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const { Bot, InlineKeyboard } = require('grammy');
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
+//
 bot.command('start', async (ctx) => {
   // await ctx.reply('Привет! Я - бот');
   const keyboard = new InlineKeyboard()
@@ -19,7 +20,7 @@ bot.command('start', async (ctx) => {
   });
 });
 
-// bot.on('message', (ctx) => ctx.reply('hello'));
+//
 bot.callbackQuery('button_N2', async (ctx) => {
   const keyboard = new InlineKeyboard()
     .text('Лазерная резка', 'button_laser')
@@ -27,11 +28,19 @@ bot.callbackQuery('button_N2', async (ctx) => {
     .text('Пищевая', 'button_eda')
     .row()
     .text('Электроника', 'button_electro')
+    .row()
+    .text('Нефтехимия', 'button_neft')
+    .row()
+    .text('Лаборатория', 'button_laba')
+    .row()
+    .text('Другое', 'button_other')
     .row();
-
-  await ctx.reply('Выберите вашу отрасль применения:', {
-    reply_markup: keyboard,
-  });
+  await ctx.reply(
+    'Выберите вашу отрасль применения оборудования, мы примерно поймем параметры нужного оборудования:',
+    {
+      reply_markup: keyboard,
+    },
+  );
 });
 bot.callbackQuery('button_laser', async (ctx) => {
   const keyboard = new InlineKeyboard()
@@ -40,14 +49,17 @@ bot.callbackQuery('button_laser', async (ctx) => {
     .text('НЕТ', 'button_no')
     .row();
 
-  await ctx.reply('Стоимость завист от производительности, вам они известны?', {
-    reply_markup: keyboard,
-  });
+  await ctx.reply(
+    'Стоимость узлов завист от производительности, вам известны потребляемый объем, чистота газа и давление?',
+    {
+      reply_markup: keyboard,
+    },
+  );
 });
 bot.callbackQuery('button_no', async (ctx) => {
   await ctx.reply('Введите Ваши контакты и менеджер свяжется с Вами');
   const keyboard = new InlineKeyboard()
-    .url('Позвонить нам с нашего сайта', 'https://asge.ru')
+    .url('Позвонить нам с нашего сайта', 'https://agse.ru')
     .row();
 
   await ctx.reply('📞', {
@@ -58,10 +70,14 @@ bot.on('message', async (ctx) => {
   console.log(ctx.message);
 });
 
-bot.callbackQuery('button_O2', async (ctx) => {
-  await ctx.answerCallbackQuery({ text: 'press O2' });
-  // await ctx.editMessageText('press');
+bot.callbackQuery('button_yes', async (ctx) => {
+  await ctx.reply(
+    '1. Введите, какой объем потребления в час (например 10 литров или 3 куба)?',
+  );
+  await ctx.reply('2. Введите чистоту азота необходимую.');
+  await ctx.reply('3. Введите давление в системе.');
 });
+
 bot.callbackQuery('button_vod', async (ctx) => {
   await ctx.answerCallbackQuery({ text: 'press vod' });
   // await ctx.editMessageText('press');
